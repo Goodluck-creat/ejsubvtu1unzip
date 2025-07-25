269079ada01cfacad4fea75b8a4afe3e5f9b89f1
Nwimo Goodluck Chioma <157001681+Goodluck-creat@users.noreply.github.com>
2025-07-25 01:55:23 -0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,9 +4,9 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 // DB config (adjust credentials)
 $host = 'localhost';
-$user = 'u900143872_Ej_sub';
-$pass = 'Chukwuoma1@';
-$dbname = 'u900143872_Ej_sub';
+$user = '';
+$pass = '';
+$dbname = '';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
